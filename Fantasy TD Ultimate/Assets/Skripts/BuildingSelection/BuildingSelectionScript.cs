8f8d373ec95a8024e93f1b7d23bc6bbbc760673f
YannickLehmann <yannick.lehmann@studium.uni-hamburg.de>
2019-01-25 23:48:05 +0100

    Wand and Spellbook added

---
+++ Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/BuildingSelectionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSelectionScript : MonoBehaviour {
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Build"))
+        {
+            other.GetComponent<BuildGetsCollected>().GotSelected();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<BuildGetsCollected>().GotDeSelected();
+    }
+}
