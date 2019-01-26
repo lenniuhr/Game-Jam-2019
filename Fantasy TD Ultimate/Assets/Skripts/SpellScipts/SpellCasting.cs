ca94b9f3cb034201ef6dfcd7d182275fbc9d5419
YannickLehmann <yannick.lehmann@studium.uni-hamburg.de>
2019-01-26 16:54:34 +0100

    SpellBomb

--- Fantasy TD Ultimate/Assets/Skripts/SpellScipts/SpellCasting.cs
+++ Fantasy TD Ultimate/Assets/Skripts/SpellScipts/SpellCasting.cs
@@ -11,9 +11,18 @@ public class SpellCasting : MonoBehaviour {
     private int SpellIndex = 0;
     private GameObject SelectedSpell;
     private GameObject HoldingSpell;
+    private GameObject SpellBomb;
     private bool trigger = false;
+    private bool HoldingBomb = false;
     private Vector3 Aim;
 
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+    private void Awake()
+    {
+        trackedObj = GetComponentInParent<SteamVR_TrackedObject>();
+    }
 
     // Wenn auf ein Spell gezeigt wird gebe Feedback und speicher das entsprechende Gebäude
     private void OnTriggerEnter(Collider other)
@@ -27,6 +36,10 @@ public class SpellCasting : MonoBehaviour {
             else if (other.gameObject.tag.Contains("2"))
             {
                 SpellIndex = 1;
+            }
+            else if (other.gameObject.tag.Contains("3"))
+            {
+                SpellIndex = 2;
             }
             other.GetComponent<SpellGetsCollected>().GotSelected();
             SelectedSpell = other.gameObject;
@@ -50,8 +63,14 @@ public class SpellCasting : MonoBehaviour {
         {
             Debug.Log("BuildingSelected");
             trigger = true;
-            HoldingSpell = Instantiate(Spell[SpellIndex]) as GameObject;
-
+            if (SpellIndex == 2){
+                SpellBomb = Instantiate(Spell[SpellIndex]) as GameObject;
+                SpellBomb.transform.parent = this.transform;
+                SpellBomb.GetComponent<Transform>().localPosition = new Vector3(0f, 0.3f, 0f);
+                HoldingBomb = true;
+            } else {
+                HoldingSpell = Instantiate(Spell[SpellIndex]) as GameObject;
+           
             HoldingSpell.transform.parent = this.transform;
             //HoldingBuilding.GetComponent<Transform>().position = this.GetComponent<Transform>().position;
             // HoldingBuilding.GetComponent<Transform>().position = new Vector3(HoldingBuilding.GetComponent<Transform>().position.x, HoldingBuilding.GetComponent<Transform>().position.y + 0.3f, HoldingBuilding.GetComponent<Transform>().position.z);
@@ -59,7 +78,9 @@ public class SpellCasting : MonoBehaviour {
             HoldingSpell.GetComponent<Transform>().localPosition = new Vector3(0f, 0.3f, 0f);
 
             //Activiere den Raycast
-            Ray.SetActive(true);
+
+                Ray.SetActive(true);
+            }
         }
 
     }
@@ -90,6 +111,10 @@ public class SpellCasting : MonoBehaviour {
                 yield return new WaitForSeconds(.2f);
             }
 
+        }else if (SpellIndex == 2)
+        {
+            SpellBomb.transform.parent = null;
+           HoldingBomb = false;
         }
         Destroy(HoldingSpell);
         Ray.SetActive(false);
@@ -111,9 +136,23 @@ public class SpellCasting : MonoBehaviour {
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(0f, 1f, 0f) * 200, Color.yellow);
             Aim = transform.TransformDirection(0f, 1f, 0f) * 400;
+            if (HoldingBomb)
+            {
+                Debug.Log("Holding");
+                SpellBomb.GetComponent<Transform>().localPosition = new Vector3(0f, 0.3f, 0f);
+                device = SteamVR_Controller.Input((int)trackedObj.index);
+                SpellBomb.GetComponent<Rigidbody>().velocity = device.velocity*4;
+                SpellBomb.GetComponent<Rigidbody>().angularVelocity = device.angularVelocity;
 
-
+            }
         }
+        
+    }
+
+    public void FixedUpdate()
+    {
+
+        
     }
 
 }
