c0409627b1010c710b9d5003efc12dfd56b5ee50
YannickLehmann <yannick.lehmann@studium.uni-hamburg.de>
2019-01-26 10:25:43 +0100

    Selection Orb

--- Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/BuildingSelectionScript.cs
+++ Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/BuildingSelectionScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BuildingSelectionScript : MonoBehaviour {
 
+    public GameObject[] BuildEffect;
     private GameObject SelectedBuilding;
     private GameObject HoldingBuilding;
+    private int BuildIndex = 0;
 
     private bool trigger = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Build"))
+        if (other.gameObject.tag.Contains("Build"))
         {
+            if (other.gameObject.tag.Contains("1"))
+            {
+                BuildIndex = 0;
+            } else if (other.gameObject.tag.Contains("2"))
+            {
+                BuildIndex = 1;
+            }
             other.GetComponent<BuildGetsCollected>().GotSelected();
             SelectedBuilding = other.gameObject;
         }
@@ -20,8 +29,11 @@ public class BuildingSelectionScript : MonoBehaviour {
 
     private void OnTriggerExit(Collider other)
     {
-        other.GetComponent<BuildGetsCollected>().GotDeSelected();
-        SelectedBuilding = null;
+        if (SelectedBuilding)
+        {
+            other.GetComponent<BuildGetsCollected>().GotDeSelected();
+            SelectedBuilding = null;
+        }
     }
 
     public void Trigger()
@@ -31,11 +43,15 @@ public class BuildingSelectionScript : MonoBehaviour {
         {
             Debug.Log("BuildingSelected");
             trigger = true;
-            HoldingBuilding = Instantiate(SelectedBuilding);
-           // HoldingBuilding.transform.parent = this.transform;
-           // HoldingBuilding.transform.position = new Vector3(0, 0, 0);
+            HoldingBuilding = Instantiate(BuildEffect[BuildIndex]) as GameObject;
+
+            HoldingBuilding.transform.parent = this.transform;
+            //HoldingBuilding.GetComponent<Transform>().position = this.GetComponent<Transform>().position;
+            // HoldingBuilding.GetComponent<Transform>().position = new Vector3(HoldingBuilding.GetComponent<Transform>().position.x, HoldingBuilding.GetComponent<Transform>().position.y + 0.3f, HoldingBuilding.GetComponent<Transform>().position.z);
+            //HoldingBuilding.GetComponent<Transform>().rotation = this.GetComponentInParent<Transform>().rotation;
+            HoldingBuilding.GetComponent<Transform>().localPosition = new Vector3(0f, 0.3f, 0f);
         }
-         
+
     }
 
     public void TriggerExit()
