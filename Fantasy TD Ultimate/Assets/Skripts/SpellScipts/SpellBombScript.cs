70de39b69b44c4f91aaa84adac103cefb212628d
YannickLehmann <yannick.lehmann@studium.uni-hamburg.de>
2019-01-26 18:59:45 +0100

    Sounds

--- Fantasy TD Ultimate/Assets/Skripts/SpellScipts/SpellBombScript.cs
+++ Fantasy TD Ultimate/Assets/Skripts/SpellScipts/SpellBombScript.cs
@@ -2,33 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpellFire : MonoBehaviour {
+public class SpellBombScript : MonoBehaviour {
 
-    public float speed = 1f;
-    public int damage = 100;
-    private Vector3 MyTarget;
-    private bool Firing = false;
-    private Vector3 OldPosition = new Vector3 (0, 0, 0);
-    public GameObject[] Effekt;
-
-    public void Fire(Vector3 Target)
-    {
-        MyTarget = Target;
-        Firing = true;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (Firing) {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, MyTarget, speed);
-            if (this.transform.position == OldPosition)
-            {
-                Destroy(this.gameObject);
-            }
-            OldPosition = this.transform.position;            
-        }
-    }
+    public GameObject[] Effects;
 
     public void OnTriggerEnter(Collider collider)
     {
@@ -38,7 +14,6 @@ public class SpellFire : MonoBehaviour {
             Debug.Log("EnemyHit");
             collider.gameObject.GetComponent<Attacker>().TakeDamage(damage);
             Destroy(gameObject);
-            GameObject EffectInstance =  Instantiate(Effekt[0]);
             EffectInstance.GetComponent<Transform>().position = this.GetComponent<Transform>().position;
             //StartCoroutine(DeleteEffect(EffectInstance));
 
@@ -56,7 +31,6 @@ public class SpellFire : MonoBehaviour {
     {
         Debug.Log("Specal Effect");
         yield return new WaitForSeconds(2f);
-       // Destroy(Effect);
+        // Destroy(Effect);
     }
-
 }
