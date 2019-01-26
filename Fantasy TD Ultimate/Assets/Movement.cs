eb8f95dac618deb6c7f37f6086dc12dfedfdf355
lenniuhr <lennartuhrmacher@gmail.com>
2019-01-26 11:38:47 +0100

    Implement towers 1

---
+++ Fantasy TD Ultimate/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveZ = -0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(0f,0f,moveZ));
+    }
+}
