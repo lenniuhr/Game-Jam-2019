9410cd42d9ce0513b75e3c27406c6dcc6ff449d3
YannickLehmann <yannick.lehmann@studium.uni-hamburg.de>
2019-01-27 02:47:49 +0100

    Hover towers

--- Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/BuildGetsCollected.cs
+++ Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/BuildGetsCollected.cs
@@ -10,7 +10,7 @@ public class BuildGetsCollected : MonoBehaviour {
 	// Use this for initialization
     public void GotSelected()
     {
-       
+        Tower.SetActive(true);
         /*
         this.transform.localScale = new Vector3(this.transform.localScale.x + SelectionScale, this.transform.localScale.y + SelectionScale, this.transform.localScale.z + SelectionScale);
         this.GetComponent<Renderer>().material = Mat[1];
@@ -19,6 +19,7 @@ public class BuildGetsCollected : MonoBehaviour {
 
     public void GotDeSelected()
     {
+        Tower.SetActive(false);
         /*
         this.transform.localScale = new Vector3(this.transform.localScale.x - SelectionScale, this.transform.localScale.y - SelectionScale, this.transform.localScale.z - SelectionScale);
         this.GetComponent<Renderer>().material = Mat[0];
---
+++ Fantasy TD Ultimate/Assets/Skripts/BuildingSelection/RotationSkript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSkript : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.position, new Vector3(0, 1, 0), Time.deltaTime * 90f);
+    }
+}
